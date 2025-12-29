f8de72d357200187033eccd132fa163c653a9a9e
denkuservices <denkuservices@gmail.com>
2025-12-29 18:59:16 -0500

    Standardize pricing page layout

--- web/src/app/(marketing)/pricing/page.tsx
+++ web/src/app/(marketing)/pricing/page.tsx
@@ -1,5 +1,13 @@
 import { PricingTable } from '@/components/marketing/pricing-table';
+import { Container } from '@/components/marketing/Container';
+import { Section } from '@/components/marketing/Section';
 
 export default function PricingPage() {
-  return <PricingTable />;
+  return (
+    <Section>
+      <Container>
+        <PricingTable />
+      </Container>
+    </Section>
+  );
 }
